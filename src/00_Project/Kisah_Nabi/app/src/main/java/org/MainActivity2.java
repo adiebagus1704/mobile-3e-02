5cadc34b91cbcfac197d3a425b9a24c36acf6e82
adiebagus1704 <56287929+adiebagus1704@users.noreply.github.com>
2020-11-20 18:31:19 +0700

    Update src

---
+++ src/00_Project/Kisah_Nabi/app/src/main/java/org/MainActivity2.java
@@ -0,0 +1,16 @@
+package org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.aplas.kisahnabinabi.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
